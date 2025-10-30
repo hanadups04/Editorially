47de0c5ea0700d23a8890b86d06139d53ac7e257
blekism <cristianlaviano@gmail.com>
2025-10-30 21:25:33 +0800

    Refactor application structure and update dependencies
    - Updated package.json to include new dependencies for FullCalendar, React PDF Viewer, Slate, Supabase, and others, while removing outdated React and React DOM versions.
    - Removed unused App.css styles and index.css file to streamline the project.
    - Enhanced App.jsx with a ProtectedRoute component for access control based on user permissions.
    - Implemented loading states and user access level checks in ProtectedRoute.
    - Updated routing structure to include new routes and access levels for various components.
    - Cleaned up main.jsx by removing the import of the now-deleted index.css file.

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,283 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  Link,
+  Outlet,
+  useNavigate,
+} from "react-router-dom";
+import "./App.css";
+
+const ProtectedRoute = ({ requiredAccessLvl, children }) => {
+  const [user, loading] = useAuthState(auth);
+  const [userAccessLvl, setUserAccessLvl] = useState(null);
+  const navigate = useNavigate();
+  const [isAccessLvlFetched, setIsAccessLvlFetched] = useState(false);
+  const [isLoading, setIsLoading] = useState(false);
+  const { branchId, currentSlug } = useBranch();
+
+  useEffect(() => {
+    if (loading) {
+      setIsLoading(true);
+      return;
+    }
+
+    if (!user) {
+      navigate(`/${currentSlug}`);
+      console.log(currentSlug, "currnetSlug");
+
+      return;
+    }
+
+    const fetchUserAccessLvl = async () => {
+      setIsLoading(true);
+      try {
+        const userDoc = await getDoc(doc(db, "users", user.uid));
+        if (userDoc.exists()) {
+          setUserAccessLvl(userDoc.data().permissions.accessLevel);
+        } else {
+          console.log("User not found in database");
+          console.log(currentSlug, "currnetSlug");
+
+          navigate(`/${currentSlug}`);
+        }
+      } catch (error) {
+        console.error("Error during sign-in:", error.message);
+      } finally {
+        setIsAccessLvlFetched(true);
+        setIsLoading(false);
+      }
+    };
+    fetchUserAccessLvl();
+  }, [user, loading, navigate]);
+
+  useEffect(() => {
+    if (isAccessLvlFetched && !requiredAccessLvl.includes(userAccessLvl)) {
+      console.log(currentSlug, "currnetSlug");
+      navigate(`/${currentSlug}`);
+    }
+  }, [isAccessLvlFetched, userAccessLvl, requiredAccessLvl, navigate]);
+
+  if (isLoading || !isAccessLvlFetched) {
+    return (
+      <div>
+        {" "}
+        {isLoading ? (
+          <div className="loadProgress">
+            <img
+              src={LogoWhitebg}
+              alt="Loading..."
+              height={100}
+              width={100}
+              style={{ objectFit: "contain" }}
+              className="loading-logo"
+            />
+            <p className="loadingText">Loading, please wait...</p>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    );
+  }
+
+  if (!requiredAccessLvl.includes(userAccessLvl)) {
+    // useEffect(() => {
+    //   navigate("/"); // Redirect to "/"
+    // }, [navigate]);
+
+    return (
+      <div>
+        Unauthorized access. You do not have permission to view this page.
+      </div>
+    );
+  }
+
+  return children ? children : <Outlet />;
+};
 
 function App() {
-  const [count, setCount] = useState(0)
+  const withAccess = (Component, levels) => (
+    <ProtectedRoute requiredAccessLvl={levels}>
+      <Component />
+    </ProtectedRoute>
+  );
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Router>
+        <Routes>
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/Login" element={<LoginPage />} />
+          <Route path="/Register" element={<RegisterPage />} />
+          <Route
+            path="/RegisterBranch"
+            element={withAccess(RegisterBranch, [1, 2, 3, 4, 5, 6])}
+          />
+          <Route path="/:branchSlug" element={<ThemeLayoutRouter />}>
+            <Route index element={<ThemeViewRouter view="home" />} />
+            <Route
+              path=":sectionName"
+              // element={withReaderAccess(SectionPage)}
+              element={<ThemeViewRouter view="section" />}
+            />
+            <Route
+              path="/:branchSlug/Profile"
+              element={withReaderAccess(ReaderProfile)}
+            />
+
+            <Route
+              path="/:branchSlug/SavedPosts"
+              element={withReaderAccess(SavedPostPage)}
+            />
+            <Route
+              path="ViewArticle"
+              // element={withReaderAccess(ViewArticle)}
+              element={<ThemeViewRouter view="article" />}
+            />
+
+            <Route
+              path="/:branchSlug/AboutUs"
+              element={withReaderAccess(AboutUs)}
+            />
+
+            <Route
+              path="/:branchSlug/AboutUsProfile"
+              element={withReaderAccess(AboutUsProfile)}
+            />
+            <Route
+              path="/:branchSlug/Search"
+              element={withReaderAccess(SearchPage)}
+            />
+          </Route>
+
+          <Route
+            path="/Admin"
+            element={withAccess(AdminPageTemplate, [6, 5, 4, 3, 2])}
+          >
+            <Route path="Profile" element={<ReaderProfile />} />
+
+            <Route
+              path="AdminDashboard"
+              element={withAccess(AdminDashboard, [6, 5, 4])}
+            />
+            <Route
+              path="EicHomepage"
+              element={withAccess(EicHomepage, [6, 5, 4])}
+            />
+            <Route
+              path="UserManagement"
+              element={withAccess(UserManagement, [6, 5, 4, 3])}
+            />
+            <Route
+              path="ManageRoles"
+              element={withAccess(ManageRoles, [6, 5, 4])}
+            />
+            <Route
+              path="BranchProcess"
+              element={withAccess(BranchProcess, [6, 5, 4])}
+            />
+            <Route
+              path="EbHomepage"
+              element={withAccess(EbHomepage, [5, 4, 3])}
+            />
+            <Route
+              path="TopicNSubtasksView"
+              element={withAccess(TopicNSubtasksView, [6, 5, 4, 3, 2])}
+            />
+            <Route
+              path="CreateTaskPage"
+              element={withAccess(CreateTaskPage, [6, 5, 4, 3, 2])}
+            />
+            <Route
+              path="ContribHomepage"
+              element={withAccess(ContribHomepage, [2])}
+            />
+            <Route
+              path="SwHomepage"
+              element={withAccess(SwHomepage, [5, 4, 3, 2])}
+            />
+            <Route
+              path="applicantInfoPage"
+              element={withAccess(ApplicantInfoPage, [6, 5, 4, 3, 2])}
+            />
+            <Route
+              path="ContribHuntParent"
+              element={withAccess(ContribHuntParent, [6, 5, 4, 3])}
+            />
+            <Route
+              path="CmsPage"
+              element={withAccess(CmsPage, [5, 6, 4, 3, 2])}
+            />
+            <Route
+              path="ViewCmsArticle"
+              element={withAccess(ViewCmsArticle, [5, 6, 4, 3, 2])}
+            />
+            <Route
+              path="AdminCalendar"
+              element={withAccess(AdminCalendar, [5, 6, 4, 3, 2])}
+            />
+            <Route
+              path="HistoryPage"
+              element={withAccess(HistoryPage, [5, 6, 4, 3, 2])}
+            />
+          </Route>
+
+          <Route
+            path="/TextEditor"
+            element={
+              <ProtectedRoute requiredAccessLvl={[6, 5, 4, 3, 2]}>
+                <TextEditor />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/PubEditor"
+            element={
+              <ProtectedRoute requiredAccessLvl={[6, 5, 4, 3, 2]}>
+                <PubEditor />
+              </ProtectedRoute>
+            }
+          />
+
+          <Route
+            path="/SAdmin" // everything accesslevel inside to be set to 5
+            element={withAccess(SuperAdminTemplate, [4, 5])}
+          >
+            <Route
+              path="SADashboard"
+              element={withAccess(SADashboard, [4, 5])}
+            />
+            <Route
+              path="SAMembers"
+              element={withAccess(SAMemberList, [4, 5])}
+            />
+            <Route
+              path="SABranches"
+              element={withAccess(SABranchLists, [4, 5])}
+            />
+            <Route
+              path="SAApplicants"
+              element={withAccess(SABranchApplicants, [4, 5])}
+            />
+            <Route
+              path="SADeactivatedPublications"
+              element={withAccess(SADeactivatedBranchList, [4, 5])}
+            />
+
+            <Route path="SAUserLogs" element={withAccess(SAUserLogs, [4, 5])} />
+            <Route path="SAProjLogs" element={withAccess(SAProjLogs, [4, 5])} />
+            <Route
+              path="branch/:branchId"
+              element={withAccess(SingleBranchView, [4, 5])}
+            />
+          </Route>
+        </Routes>
+      </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
